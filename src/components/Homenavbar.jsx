aff7366d10d04b5153cf721ba003478ec67a9956
akash9676 <akashnaidu545@gmail.com>
2022-10-21 23:35:59 +0530

    Homenavbar

--- src/components/Homenavbar.jsx
+++ src/components/Homenavbar.jsx
@@ -4,12 +4,12 @@ import Darkmode from '../assets/darkmode.svg';
 
 const Homenavbar = () =>{
     return(
-        <div className="">
+        <div className="flex p-2 pr-16 pl-16 ">
             <div className='p-4'>
                 <img src={Logo} alt="GDSC logo" className='w-96 h-11' />
             </div>
             <div className="flex-1"></div>
-            <div className='pl-7 pr-7'>
+            <div className='p-4 pl-7 pr-7'>
                 <img src={Darkmode} alt="GDSC logo" className='w-10 h-9' />
             </div>
         </div>
